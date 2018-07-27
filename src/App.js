330fdac27c58f9f0321c773ad7268326efdab31d
Saerom Hong <hello@romcreative.com>
2018-07-27 16:54:06 -0400

    Adds basic stylesheets, cassette image, starts chunking

--- src/App.js
+++ src/App.js
@@ -5,69 +5,54 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Barn />
-      </div>
-    );
-  }
-}
-
-class Barn extends Component {
-  renderLabel(qty,word) {
-    return (
-      <span>{[...Array(qty).keys()].map( ()=> {
-        return word + " ";
-      })}</span>
+      <React.Fragment>
+        <Header />
+        <AlphabetMenu />
+      </React.Fragment>
     )
   }
+}
+
+class AlphabetMenu extends Component {
   render() {
     return (
-      <div className="barn">
-        { this.renderLabel(50, "barn") }
-        <Chicken renderLabel={this.renderLabel} />
+      <React.Fragment>
+        <div className="alphabet-menu-wrapper">A B C D E F G</div>
+      </React.Fragment>
+    )
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="logo"></div>
+          <h1 className="App-title">For Promotional Use Only</h1>
+          <div className='slogan'>Classic Rave Music from the 90s and Beyond</div>
+        </header>
+        <Row />
       </div>
     );
   }
 }
 
-class Chicken extends Component {
+class Row extends Component {
   render() {
     return (
-      <div className="chicken">
-        { this.props.renderLabel(50, "chicken") }
-        <Egg renderLabel={this.props.renderLabel} />
-      </div>
-    );
+      <React.Fragment>
+        <div className="row">
+          <div className="pink-border col-xs-6 col-sm-3">.col-xs-6 .col-sm-3</div>
+          <div className="pink-border col-xs-6 col-sm-3">.col-xs-6 .col-sm-3</div>
+
+          <div className="clearfix visible-xs-block"></div>
+
+          <div className="pink-border col-xs-6 col-sm-3">.col-xs-6 .col-sm-3</div>
+          <div className="pink-border col-xs-6 col-sm-3">.col-xs-6 .col-sm-3</div>
+        </div>
+      </React.Fragment>
+    )
   }
 }
-
-class Egg extends Component {
-  render() {
-    return (
-      <div className="egg">
-        { this.props.renderLabel(50, "egg") }
-        <Chick renderLabel={this.props.renderLabel} />
-      </div>
-    );
-  }
-}
-
-class Chick extends Component {
-  render() {
-    return (
-      <div className="chick">
-        { this.props.renderLabel(25, "cheep") }
-      </div>
-    );
-  }
-}
-
-
 export default App;
