f5ae341705bffe42de2d058a122c0d6d164a6d33
Saerom Hong <hello@romcreative.com>
2018-07-29 11:00:31 -0400

    Adds root (/) route with React-Router-Dom

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App'; // using Router instead of App now
+import Router from './Router';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router />, document.getElementById('root'));
 registerServiceWorker();
